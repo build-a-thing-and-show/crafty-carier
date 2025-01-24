8ba736c56a6444fb4a48da1d02773445adea3e32
Reza <rzgholizadeh@gmail.com>
2025-01-23 16:47:13 -0800

    Change root get response message

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
     def index
-      render json: { message: 'hello world' }
+      render json: { message: 'The Crafty-Carier Server is up and running. No issues whatsoever.' }
     end
   end
