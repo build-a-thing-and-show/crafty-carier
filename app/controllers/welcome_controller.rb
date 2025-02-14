f42bf354974bf5db7203144c8b9ef8f6b2cfb64a
Reza <rzgholizadeh@gmail.com>
2025-02-14 14:38:38 -0800

    Delete ruby project

--- app/controllers/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class WelcomeController < ApplicationController
-    def index
-      render json: { message: "The Crafty-Carier Server is up and running. No issues whatsoever." }
-    end
-end
--- config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get "/", to: "welcome#index"
-  namespace :api do
-    post "hellome", to: "hellome#hello"
-  end
-end
